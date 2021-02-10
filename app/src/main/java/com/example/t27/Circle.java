2e746e905a95384eb2d98eb38a5887a65be0a4c9
osmani2 <nahmed2@ualberta.ca>
2021-02-09 19:08:47 -0700

    Added Circle class
    Extends shape

---
+++ app/src/main/java/com/example/t27/Circle.java
@@ -0,0 +1,12 @@
+package com.example.t27;
+
+public class Circle extends Shape{
+
+    private int x;
+    private int y;
+
+    public Circle(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+}
